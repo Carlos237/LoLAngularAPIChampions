de106984641ad2f77e0bffa82b52e0ccaba244e6
Carlos237 <cgv237@gmail.com>
2021-11-15 17:10:02 +0100

    Added some things to github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: "http://ddragon.leagueoflegends.com/cdn/11.20.1/data/es_ES/championFull.json",
+  ChampsUrl: "http://ddragon.leagueoflegends.com/cdn/11.19.1/data/es_ES/champion"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 import { ChampsTableComponent } from "src/app/components/champs-table/champs-table.component";
 
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: "http://ddragon.leagueoflegends.com/cdn/11.20.1/data/es_ES/championFull.json",
   ChampsUrl: "http://ddragon.leagueoflegends.com/cdn/11.19.1/data/es_ES/champion"
 };
